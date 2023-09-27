0787351cabbe893d3fa63e8d8cc0f8a65b3b10bd
matheusrodrigues1 <matech00@outlook.com>
2023-09-27 18:26:30 -0300

    finalização do projeto

--- src/components/SectionServices.tsx
+++ src/components/SectionServices.tsx
@@ -1,16 +1,67 @@
 import  Container  from './Container'
+import ImagePhone from "@/assets/phone.png"
+import SoluctionsIcon from "@/assets/icon-solutions.svg"
+import OptionsIcon from "@/assets/icon-options.svg"
+import CardIcon from "@/assets/icon-card.svg"
+import PhoneIcon from "@/assets/icon-phone.svg"
+import Image from 'next/image'
 import React from 'react'
 
 function SectionServices() {
   return (
-    <section>
+    <section className='relative w-full h-[965px]'>
       <Container>
-        <div>
-          <span>serviços exclusivos</span>
-          <h1>Gerencie suas finanças sem sair de casa</h1>
-          <p>Veja como você pode cuidar das suas finanças pelo app Itaú de froma segura, rápida e o melhor, no conforto da sua casa.</p>
+        <div className='flex-1 max-w-[594px] pt-32'>
+          <span className='block text-primary-orange text-sm font-bold uppercase mb-9'>serviços exclusivos</span>
+          <h2 className='text-primary-gray text-[56px] font-bold leading-tight mb-6'>Gerencie suas finanças sem sair de casa</h2>
+          <p className='text-lg max-w-[554px] mb-16 text-second-gray'>Veja como você pode cuidar das suas finanças pelo app Itaú de froma segura, rápida e o melhor, no conforto da sua casa.</p>
+          <ul className='flex flex-col items-start gap-9'>
+            <li className='flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray'>
+              <div className='w-7 h-7 flex items center justify-center'>
+                <Image
+                  src={PhoneIcon}
+                  alt='icon phone'
+                />
+              </div>
+              <p className='flex-1 text-txt-gray pr-2'>Acompanhar sua conta, fazer transferências e pagamentos de onde estiver</p>
+            </li>
+            <li className='flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray'>
+              <div className='w-7 h-7 flex items center justify-center'>
+                <Image
+                  src={SoluctionsIcon}
+                  alt='icon phone'
+                />
+              </div>
+              <p className='flex-1 text-txt-gray pr-2'>Acompanhar sua conta, fazer transferências e pagamentos de onde estiver</p>
+            </li>
+            <li className='flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray'>
+              <div className='w-7 h-7 flex items center justify-center'>
+                <Image
+                  src={OptionsIcon}
+                  alt='icon phone'
+                />
+              </div>
+              <p className='flex-1 text-txt-gray pr-2'>Acompanhar sua conta, fazer transferências e pagamentos de onde estiver</p>
+            </li>
+            <li className='flex items-center gap-9'>
+              <div className='w-7 h-7 flex items center justify-center'>
+                <Image
+                  src={CardIcon}
+                  alt='icon phone'
+                />
+              </div>
+              <p className='flex-1 text-txt-gray pr-2'>Acompanhar sua conta, fazer transferências e pagamentos de onde estiver</p>
+            </li>
+          </ul>
         </div>
       </Container>
+      <div className='absolute top-0 right-0 flex items-center w-[32%] h-full  bg-gray-phone'>
+        <Image
+          src={ImagePhone}
+          alt='phone'
+          className='translate-x-[-50%]'
+        />
+      </div>
     </section>
   )
 }
